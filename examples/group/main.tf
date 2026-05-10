248d80a237a83c6fcf65dec83cdef19bb2ccc482
dennismdejong <dennis@caberdo.nl>
2026-05-10 18:28:10 +0200

    docs: add examples for virtual_folders in groups
    This addresses issue #19 by providing clear documentation on how to
    correctly use virtual_folders in groups:
    
    1. Inline folder definition with required mapped_path and filesystem
    2. Separate sftpgo_folder resource with reference by name
    
    The original issue occurred because users were not providing the
    required mapped_path and filesystem fields when defining inline
    virtual folders.
    
    Changes:
    - examples/group/main.tf: Added required fields comment and
      mapped_path/filesystem to the example
    - examples/group/with_inline_folder.tf: New example showing complete
      inline folder definition
    - examples/group/with_folder_resource.tf: New example showing folder
      as separate resource with reference

--- examples/group/main.tf
+++ examples/group/main.tf
@@ -17,19 +17,27 @@ resource "sftpgo_group" "test" {
     user_settings = {
         max_sessions = 10
         filters = {
-          denied_protocols = ["FTP"]
-          web_client = ["write-disabled", "password-change-disabled"]
+            denied_protocols = ["FTP"]
+            web_client = ["write-disabled", "password-change-disabled"]
         }
         filesystem = {
             provider = 0
         }
     }
+    # When using inline virtual_folders, you must provide:
+    # - name, virtual_path, quota_size, quota_files (as before)
+    # - mapped_path: absolute path on the filesystem (REQUIRED!)
+    # - filesystem: storage provider configuration (REQUIRED!)
     virtual_folders = [
       {
-        name = "test"
+        name        = "test"
         virtual_path = "/g1"
-        quota_size = 0
-        quota_files = 0
+        quota_size   = 0
+        quota_files  = 0
+        mapped_path  = "/srv/sftpgo/test"
+        filesystem = {
+            provider = 0
+        }
       }
     ]
 }
